0fe7a3f3a3c616791ad5b4e9b559c034285c27f7
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-09-02 02:10:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
     @artist = artist 
     @genre = genre
     @@count += 1
-    # if !(ARTISTS.include?(@artist))
+    # if !(@artists.include?(@artist))
     #   ARTISTS << @artist 
     # end
   
